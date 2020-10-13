14a8d9f408677e8951e89ebf6b06effbaab0bc0e
rachel <rachelkiffiak1@gmail.com>
2020-10-12 20:49:37 -0600

    Setting up the Tweet format.

---
+++ src/components/Tweet.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <h3>Tweety tweet</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "page-tweet",
+        props: {
+            tweetObject:{
+                type: Object,
+                required: true
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
